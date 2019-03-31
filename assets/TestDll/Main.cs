b2d719adbae75a0986f341609600f2c72de18453
yemi <zcyemi@gmail.com>
2019-03-31 14:03:01 +0800

    Add sample .netcore project.

---
+++ assets/TestDll/Main.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TestDll
+{
+    public class Main
+    {
+        public static int add(int a,int b){
+            return a+b;
+        }
+    }
+}
---
+++ assets/TestDll/build.bat
@@ -0,0 +1,2 @@
+dotnet build
+xcopy /Y .\bin\Debug\netstandard2.0\TestDll.dll .\..\
